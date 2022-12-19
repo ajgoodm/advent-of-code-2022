92931e3a94791dc07e671cd69805ace8d2c71d3c
Aaron Goodman <aarongoodm@gmail.com>
2022-12-19 10:12:15 -0500

    code up logic

--- src/day_19/src/main.rs
+++ src/day_19/src/main.rs
@@ -13,20 +13,6 @@ lazy_static! {
     ).unwrap();
 }
 
-enum RobotType {
-    OreCollecting,
-    ObsidianCollecting,
-    ClayCollecting,
-    GeodeCracking,
-}
-
-enum ResourceType {
-    Ore,
-    Clay,
-    Obsidian,
-    CrackedGeode,
-}
-
 struct ResourceCost {
     n_ore: usize,
     n_clay: usize,
@@ -39,6 +25,105 @@ struct Blueprint {
     clay_collector_cost: ResourceCost,
     obsidian_collector_cost: ResourceCost,
     geode_cracker_cost: ResourceCost,
+}
+
+#[derive(PartialEq, Eq, Hash, Clone)]
+struct SimulationState {
+    n_ore_possessed: usize,
+    n_clay_possessed: usize,
+    n_obsidian_possessed: usize,
+    n_geodes_cracked: usize,
+    n_ore_collectors_built: usize,
+    n_obsidian_collectors_built: usize,
+    n_clay_collectors_built: usize,
+    n_geode_crackers_built: usize,
+}
+
+impl SimulationState {
+    fn new_simulation() -> SimulationState {
+        SimulationState {
+            n_ore_possessed: 0,
+            n_clay_possessed: 0,
+            n_obsidian_possessed: 0,
+            n_geodes_cracked: 0,
+            n_ore_collectors_built: 1,
+            n_obsidian_collectors_built: 0,
+            n_clay_collectors_built: 0,
+            n_geode_crackers_built: 0,
+        }
+    }
+
+    fn can_afford_robot(&self, resource_cost: &ResourceCost) -> bool {
+        self.n_ore_possessed >= resource_cost.n_ore
+            && self.n_clay_possessed >= resource_cost.n_clay
+            && self.n_obsidian_possessed >= resource_cost.n_obsidian
+    }
+
+    fn buy_robot(&mut self, resource_cost: &ResourceCost) {
+        self.n_ore_possessed -= resource_cost.n_ore;
+        self.n_clay_possessed -= resource_cost.n_clay;
+        self.n_obsidian_possessed -= resource_cost.n_obsidian;
+    }
+
+    fn buy_ore_collector(&self, blueprint: &Blueprint) -> SimulationState {
+        let mut new_simulation = self.clone();
+        new_simulation.buy_robot(&blueprint.ore_collector_cost);
+        new_simulation.n_ore_collectors_built += 1;
+        new_simulation
+    }
+
+    fn buy_clay_collector(&self, blueprint: &Blueprint) -> SimulationState {
+        let mut new_simulation = self.clone();
+        new_simulation.buy_robot(&blueprint.clay_collector_cost);
+        new_simulation.n_clay_collectors_built += 1;
+        new_simulation
+    }
+
+    fn buy_obsidian_collector(&self, blueprint: &Blueprint) -> SimulationState {
+        let mut new_simulation = self.clone();
+        new_simulation.buy_robot(&blueprint.obsidian_collector_cost);
+        new_simulation.n_obsidian_collectors_built += 1;
+        new_simulation
+    }
+
+    fn buy_geode_cracker(&self, blueprint: &Blueprint) -> SimulationState {
+        let mut new_simulation = self.clone();
+        new_simulation.buy_robot(&blueprint.geode_cracker_cost);
+        new_simulation.n_geode_crackers_built += 1;
+        new_simulation
+    }
+
+    /// Given the robots we have, update our resources
+    fn collect_resources(&mut self) {
+        self.n_ore_possessed += self.n_ore_collectors_built;
+        self.n_clay_possessed += self.n_clay_collectors_built;
+        self.n_obsidian_possessed += self.n_obsidian_collectors_built;
+        self.n_geodes_cracked += self.n_geode_crackers_built;
+    }
+
+    /// Given the resources available, what could we do next?
+    fn next_possible_states(&self, blueprint: &Blueprint) -> Vec<SimulationState> {
+        let mut next_state_noop = self.clone();
+        next_state_noop.collect_resources();
+
+        let mut next_states: Vec<SimulationState> = Vec::new();
+        if self.can_afford_robot(&blueprint.ore_collector_cost) {
+            next_states.push(next_state_noop.buy_ore_collector(&blueprint));
+        }
+        if self.can_afford_robot(&blueprint.clay_collector_cost) {
+            next_states.push(next_state_noop.buy_clay_collector(&blueprint));
+        }
+        if self.can_afford_robot(&blueprint.obsidian_collector_cost) {
+            next_states.push(next_state_noop.buy_obsidian_collector(&blueprint));
+        }
+        if self.can_afford_robot(&blueprint.geode_cracker_cost) {
+            next_states.push(next_state_noop.buy_geode_cracker(&blueprint));
+        }
+        // we don't have to buy anything... sometimes we can't!
+        let mut next_states = vec![next_state_noop];
+
+        next_states
+    }
 }
 
 fn _parse_cost(cost_str: &str) -> ResourceCost {
