85c9e95ee0a685df40c198667eba18a2571d4f38
Aaron Goodman <aarongoodm@gmail.com>
2023-01-10 10:00:43 -0500

    snafu to decimal

--- src/day_25/src/main.rs
+++ src/day_25/src/main.rs
@@ -1,6 +1,5 @@
-fn snafu_to_decimal(s: String) -> usize {
-    let val: isize = s
-        .chars()
+fn snafu_to_decimal(s: String) -> isize {
+    s.chars()
         .rev()
         .enumerate()
         .map(|(idx, c)| match c {
@@ -11,10 +10,7 @@ fn snafu_to_decimal(s: String) -> usize {
             '=' => -2 * 5isize.pow(idx.try_into().unwrap()),
             _ => panic!("unexpected char {}", c),
         })
-        .sum();
-
-    assert!(val > 0);
-    val as usize
+        .sum()
 }
 
 fn main() {
@@ -27,11 +23,11 @@ mod tests {
 
     #[test]
     fn test_snafu_to_decimal() {
-        assert_eq!(snafu_to_decimal("1=".to_string()), 3usize);
-        assert_eq!(snafu_to_decimal("1=11-2".to_string()), 2022usize);
+        assert_eq!(snafu_to_decimal("1=".to_string()), 3isize);
+        assert_eq!(snafu_to_decimal("1=11-2".to_string()), 2022isize);
         assert_eq!(
             snafu_to_decimal("1121-1110-1=0".to_string()),
-            314159265usize
+            314159265isize
         );
     }
 }
